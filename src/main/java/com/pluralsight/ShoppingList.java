f2ea34c792701dfad2a0055682fa7dd7002d1852
penguinpants <124331070+penguinpants@users.noreply.github.com>
2026-04-07 08:55:52 -0400

    Added poptarts

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
 public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Banana");
+        System.out.println("Milk");
+        System.out.println("Eggs");
+        System.out.println("Rice");
+        System.out.println("Cereal");
+        System.out.println("Cheese");
+        System.out.println("Ham");
+        System.out.println("Tomato");
+        System.out.println("Potatoes");
+        System.out.println("Pickles");
+        System.out.println("PopTarts");
+    }
 }
